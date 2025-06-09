37d3b7c70688cc73f15c73564298191e2aeea614
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-06-10 03:42:55 +0530

    homes screen

--- lib/presentation/screens/HomesScreen.dart
+++ lib/presentation/screens/HomesScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'ExploreScreen.dart';
 import '../widgets/search_box.dart';
 class HomesScreen extends StatelessWidget {
@@ -22,33 +23,48 @@ class HomesScreen extends StatelessWidget {
             ),
 
             Column(
-              children: [
-                Stack(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(25),
-                      child: Image(
-                        height: srcwidth*0.42,
-                        width: srcwidth*0.45,
-                        image: NetworkImage("https://dynamic-media-cdn.tripadvisor.com/media/photo-o/05/d2/48/25/puputan-square.jpg?w=900&h=500&s=1"),
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(25),
+                    child: Image(
+                      height: srcwidth*0.42,
+                      width: srcwidth*0.45,
+                      image: NetworkImage("https://dynamic-media-cdn.tripadvisor.com/media/photo-o/05/d2/48/25/puputan-square.jpg?w=900&h=500&s=1"),
 
-                      fit: BoxFit.cover,
-                      ),
+                    fit: BoxFit.cover,
                     ),
-                    
-                    Positioned(
-                        right: 0
-                        ,child: IconButton(
-                      icon: Icon(Icons.favorite),
-                      iconSize: 26,
-                      onPressed: (){},
-                      color: Color.fromRGBO(54, 54, 66,0.6),
+                  ),
 
-                    ))
-                  ],
-                )
-              ],
-            )
+                  Positioned(
+                      right: 0
+                      ,child: IconButton(
+                    enableFeedback: false,
+
+
+
+                    icon: Icon(Icons.favorite),
+                    iconSize: 26,
+                    onPressed: (){
+                    },
+                    color: Color.fromRGBO(54, 54, 66,0.6),
+
+                  )),
+
+                ],
+              ),
+              Padding(
+                padding:  EdgeInsets.only(left: 8.0,top: 3),
+                child: Text("Hotel in Greater Noida",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
+              ),
+              Padding(
+                padding:  EdgeInsets.only(left: 8.0),
+                child: Text("\$35 for 2 nights",style: TextStyle(fontSize: 14,color: Colors.grey),textAlign: TextAlign.center,
+              ),)
+            ],
+                          )
             
 
 
