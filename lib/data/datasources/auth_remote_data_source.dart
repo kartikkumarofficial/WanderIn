810fccb67ca98041702c07783a6369bca369ae40
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-06-17 01:20:03 +0530

    setting up auth data layer

---
+++ lib/data/datasources/auth_remote_data_source.dart
@@ -0,0 +1,43 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class AuthRemoteDataSource {
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+    required String confirmPassword,
+  });
+
+  Future<String> logInWithEmailPassword({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final SupabaseClient supabaseClient;
+
+  AuthRemoteDataSourceImpl(this.supabaseClient);
+
+  @override
+  Future<String> logInWithEmailPassword({
+    required String email,
+    required String password,
+  }) {
+    // TODO: implement logInWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+    required String confirmPassword,
+  }) {
+    // TODO: implement signUpWithEmailPassword
+    throw UnimplementedError();
+  }
+}
