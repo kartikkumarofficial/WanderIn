f70bd4d513adc46061ae8e2bc78f3a10da346f1d
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-06-19 02:11:16 +0530

    fixing auth - made auth event class abstract

--- lib/presentation/bloc/auth_event.dart
+++ lib/presentation/bloc/auth_event.dart
@@ -1,13 +1,18 @@
 part of 'auth_bloc.dart';
 
 @immutable
-sealed class AuthEvent {}
+abstract class AuthEvent {}
 
-final class AuthSignUp extends AuthEvent{
+final class AuthSignUp extends AuthEvent {
   final String email;
   final String password;
   final String name;
   final String confirmPassword;
 
-  AuthSignUp({required this.email, required this.password, required this.name, required this.confirmPassword});
+  AuthSignUp({
+    required this.email,
+    required this.password,
+    required this.name,
+    required this.confirmPassword,
+  });
 }
